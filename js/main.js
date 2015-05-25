f879cade27bceccc57b4f20f399a119d2d40d959
Dylan <dylan.cobean@gmail.com>
2015-05-25 12:13:48 -0700

    Added: map layers, draft legend, map layer display controls

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 // map refers to a ><div element with the ID XXXXXX
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ';
-var map = L.mapbox.map('XXXXXX', 'mapbox.dark')
-    .setView([40, -74.50], 5);
+var map = L.mapbox.map('XXXXXX', 'mapbox.dark' ).setView([40, -74.50], 5);
+L.control.layers({}, {
+    'Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk'),
+    'Forest Conservation': L.mapbox.tileLayer('dylanc.Conservation'),
+    'Forest Restoration': L.mapbox.tileLayer('dylanc.Restoration')
+}).addTo(map);
 
+map.legendControl.addLegend(document.getElementById('legend').innerHTML);
 
