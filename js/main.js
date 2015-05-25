cb6b6f3d8d514c8e0b9179d9795b9a3f7a3e82a0
Dylan <dylan.cobean@gmail.com>
2015-05-25 03:55:33 -0700

    Map architecture operational

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// map refers to a ><div element with the ID XXXXXX
+L.mapbox.accessToken = 'pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ';
+var map = L.mapbox.map('XXXXXX', 'mapbox.dark')
+    .setView([40, -74.50], 5);
+
+
