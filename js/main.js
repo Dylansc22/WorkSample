b1281789d4a52642c19ff823cfd03191905e6c20
Dylan <dylan.cobean@gmail.com>
2015-06-01 23:02:44 -0700

    Added Erosion Risk scores by Nation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 // map refers to a ><div element with the ID XXXXXX
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ';
-var map = L.mapbox.map('XXXXXX', 'mapbox.dark' ).setView([40, -74.50], 5);
+var map = L.mapbox.map('XXXXXX', 'dylanc.61e6f75f' ).setView([40, -74.50], 4);
 L.control.layers({}, {
     'Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk'),
     'Forest Conservation': L.mapbox.tileLayer('dylanc.Conservation'),
-    'Forest Restoration': L.mapbox.tileLayer('dylanc.Restoration')
+    'Forest Restoration': L.mapbox.tileLayer('dylanc.Restoration'),
+	'National Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk_Nation'),
 }).addTo(map);
 
+var ErosionRiskNationVar = L.mapbox.featureLayer()
+    .addTo(map);
+
+// loads markers from the map `examples.map-0l53fhk2` on Mapbox,
+// if that map has markers
+featureLayer.loadID('dylanc.088863c7');
+
 map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+
+
 
