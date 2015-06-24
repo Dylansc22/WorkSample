87e970b5f00bcc2d917841ba7045005f8d7ad1fa
Dylan <dylan.cobean@gmail.com>
2015-06-24 01:08:20 -0700

    added stylized geojson (which I cannot yet toggle off)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // map refers to a ><div element with the ID XXXXXX
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ';
-var map = L.mapbox.map('XXXXXX', 'dylanc.61e6f75f' ).setView([40, -74.50], 4);
+var map = L.mapbox.map('mapid', 'dylanc.61e6f75f' ).setView([40, -74.50], 4);
 L.control.layers({}, {
     'Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk'),
     'Forest Conservation': L.mapbox.tileLayer('dylanc.Conservation'),
@@ -9,6 +9,43 @@ L.control.layers({}, {
 }).addTo(map);
 
 map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+
+var featLayer = L.mapbox.featureLayer().addTo(map);
+
+featLayer.loadURL('ErosionRisk_Nation.geojson');
+// inspired by http://geosprocket.github.io/btv-geographic/social/sociallayers.js 
+function getMyColor(d) {
+  return d > 7 ? '#6b0000' : 
+        d > 6 ? '#872a08' : 
+        d > 5 ? '#c46d1b' : 
+        d > 4 ? '#f5af36' : 
+        d > 3 ? '#fcd75b' : 
+        d > 1 ? '#fce96d' : 
+        '#ffff80';
+}
+
+function setStyle(feature) {
+  return {
+    weight: 0,
+    opacity: 0,
+    fillOpacity: 0.7,
+    fillColor: getMyColor(polygon.feature.properties.MAX)
+  };
+}
+
+// styles each polygon based on its properties in the geojson file, using leaflet's setStyle
+featLayer.on('ready', function() {
+    featLayer.eachLayer(function(polygon) {
+        polygon.bindPopup('This is the nation of <b>' + polygon.feature.properties.CNTRY_NAME + '</b>. and its Erosion Score is <b>' + polygon.feature.properties.MAX + '</b>');
+        console.log(typeof setStyle);
+        polygon.setStyle({
+            opacity: 0.55,
+            fillOpacity: 0.55,
+            fillColor: getMyColor(polygon.feature.properties.MAX),
+            color: getMyColor(polygon.feature.properties.MAX)
+        });
+    });
+});
 
 
 L.geoJson(ErosionRisk_Nation).addTo(map);
