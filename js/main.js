9d0d7b310c630264c9e9d92b28bf8f0b595d69ae
Dylan <dylan.cobean@gmail.com>
2015-07-01 22:08:07 -0700

    adding some code from 9884941 file, attempting to add toggleable geojson layers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // map refers to a ><div element with the ID XXXXXX
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ';
 var map = L.mapbox.map('mapid', 'dylanc.61e6f75f' ).setView([40, -74.50], 4);
+
+map.addControl(L.mapbox.infoControl().addInfo('By Dylan Cobean - June 2015'));
+
+//map.addControl(L.mapbox.legendControl());
+
+/*
 L.control.layers({}, {
     'Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk'),
     'Forest Conservation': L.mapbox.tileLayer('dylanc.Conservation'),
     'Forest Restoration': L.mapbox.tileLayer('dylanc.Restoration'),
 	'National Erosion Risk': L.mapbox.tileLayer('dylanc.ErosionRisk_Nation'),
 }).addTo(map);
+*/
 
 map.legendControl.addLegend(document.getElementById('legend').innerHTML);
 var featLayer = L.mapbox.featureLayer().addTo(map);
